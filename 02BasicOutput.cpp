149a2d4806cdeaabdc5fb77a70843505b71af618
Rainel Dave R. Azures <raineldaveazures0929@gmail.com>
2024-10-03 08:36:39 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Our Lady of Lourdes College Foundation, INC." << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "Bachelor of Science in Information Technology, 1st Year" << endl;
+	cout << "Azures, Rainel Dave Rampas" << endl;
+	cout << "P-4 Alawihao, Daet, Camarines Norte" << endl; 
+	
+	return 0;
+}
