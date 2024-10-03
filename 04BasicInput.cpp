2c023cfaf92b6cbc59b7d43b9c5d3e87bae9e3b5
Rainel Dave R. Azures <raineldaveazures0929@gmail.com>
2024-10-03 11:10:55 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name, CourseYearLevel, school;
+	
+	cout << "Nickname: ";
+	cin >> name;
+	cout << "Course-Year Level: ";
+	cin >> CourseYearLevel;
+	cout << "School: ";
+	cin >> school;
+	
+	cout << "Wow congrats " << name << "!" << CourseYearLevel << " is a nice course. And you are studying in " << school << " which is one of the Center of Excellence in Tertiary Education." << endl;
+	return 0;
+}
