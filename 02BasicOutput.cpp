ab1342e4047b24cca18a9c52e11a8accdf2c9949
Rainel Dave R. Azures <raineldaveazures0929@gmail.com>
2024-10-03 10:35:53 +0800

    02BasicOutput.cpp

--- 02BasicOutput.cpp
+++ 02BasicOutput.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-	cout << "Our Lady of Lourdes College Foundation, INC." << endl;
-	cout << "College of Computer Studies" << endl;
-	cout << "Bachelor of Science in Information Technology, 1st Year" << endl;
-	cout << "Azures, Rainel Dave Rampas" << endl;
+	cout << "Our Lady of Lourdes College Foundation, INC.\n";
+	cout << "College of Computer Studies\n";
+	cout << "Bachelor of Science in Information Technology, 1st Year\n";
+	cout << "Azures, Rainel Dave Rampas\n";
 	cout << "P-4 Alawihao, Daet, Camarines Norte" << endl; 
 	
 	return 0;
