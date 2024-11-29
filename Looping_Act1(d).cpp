6ffeaac5e20f747726537c78b2091d3878e81db0
Rainel Dave R. Azures <raineldaveazures0929@gmail.com>
2024-11-29 21:34:46 +0800

    Add files via upload

---
+++ Looping_Act1(d).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int rows=8;
+    for(int i=1; i<=rows; i++){
+        for(int j=i; j>=1; j--){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+    
